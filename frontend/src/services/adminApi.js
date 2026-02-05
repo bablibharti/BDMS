85ccdd1fbaaeef734ca6f643b433569f37ecf1f5
Babli Bharti <babli.brt.projects@gmail.com>
2026-02-06 00:12:27 +0530

    Update API URL for production

--- frontend/src/services/adminApi.js
+++ frontend/src/services/adminApi.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/admin"
+  baseURL: import.meta.env.VITE_API_URL + "/api/admin",
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
-  return req;
-});
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
